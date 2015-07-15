506cdf8aa20c8485763a03ca1b4227b69d19e307
Alexander <alexs2019@mail.ru>
2015-07-16 02:04:21 +0300

    +Iteration2

---
+++ src/folderWalk.h
@@ -0,0 +1,2 @@
+//function finds all files and calls for them searching in them
+int folderWalk (std::string path, po::variables_map vm); //1 arg - a folder where to search, it's important that "/" was not at the end of path. 2 arg means recourcivity, 3 arg is for file function.
